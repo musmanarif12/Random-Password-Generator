3174e985bd514991a421cc8d3a5ca1e51edf4f7b
Muhammad Usman Arif <usmarif24@gmail.com>
2025-09-22 12:54:34 +0500

    Initial Commit : React Random Password Generator App

---
+++ README.md
@@ -0,0 +1,11 @@
+# Random Password Generator
+
+A lightweight **React** application that generates strong, secure, and completely random passwords with a single click.  
+Built with modern JavaScript and styled with a clean white-and-blue theme.
+
+---
+
+## Features
+-  **Truly Random**: Generates a new password on every click using a customizable character set.
+-  **Secure**: Includes uppercase, lowercase, numbers, and special characters by default.
+-  **Fast & Simple**: No backend required—pure client-side React.
---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import './App.css';
+function App() {
+  const Passwords = ["123456789!@#$%^&*()_-=?/><.`~"];
+  const [password, setPassword] = useState("");
+  const changePassword = () => {
+    const chars =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?";
+    const length = 12; 
+    let newPass = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      newPass += chars[randomIndex];
+    }
+    setPassword(newPass);
+  }
+  return (
+    <>
+      <div>
+        <h1>Random Password Generator</h1>
+        <h2>
+          “Generate strong, secure passwords instantly with a single click.”
+        </h2>
+        <button onClick={changePassword}>Generate Password</button>
+        {password && <p>{password}</p>}
+      </div>
+    </>
+  );
+}
+
+export default App;
